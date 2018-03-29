f33b2e20ae3be9c1dfdc1eae882c8f6437f5b9e6
aidylewis <adrian_lewis@hushmail.com>
2018-03-29 15:46:05 +0100

    test server

---
+++ server/server.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"sync/atomic"
+	"time"
+)
+
+func main() {
+	count := uint64(0)
+	tick := time.NewTicker(time.Second)
+	http.ListenAndServe(":6060", http.HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+			atomic.AddUint64(&count, 1)
+			select {
+			case <-tick.C:
+				log.Printf("Rate: %d", atomic.SwapUint64(&count, 0))
+			default:
+			}
+		},
+	))
+}
